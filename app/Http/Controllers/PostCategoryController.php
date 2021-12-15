6d9649e4fb00b10fcd62d1a33205f00d60f1ade9
Spyder-code <luaysyauqillah@gmail.com>
2021-12-15 16:45:56 +0700

    post

---
+++ app/Http/Controllers/PostCategoryController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\PostCategory;
+use Illuminate\Http\Request;
+
+class PostCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\PostCategory  $postCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PostCategory $postCategory)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\PostCategory  $postCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PostCategory $postCategory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\PostCategory  $postCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PostCategory $postCategory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\PostCategory  $postCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PostCategory $postCategory)
+    {
+        //
+    }
+}
